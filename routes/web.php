0223bc457486feb836168a53ce195ee2c0971681
Job <LordChunk@users.noreply.github.com>
2021-03-04 11:30:42 +0100

    WIP oauth implementation

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,46 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/test', function ()
+{
+    return "hello world";
+});
+
 
 Route::get('/', function () {
-    return view('welcome');
+    $query = http_build_query([
+        'client_id' => 393, // Replace with Client ID
+        'redirect_uri' => 'http://localhost/callback',
+        'response_type' => 'code',
+        'scope' => ''
+    ]);
+
+    return redirect('https://publicapi.avans.nl/oauth/saml.php?'.$query);
+});
+
+Route::get('/callback', function (Request $request) {
+    $response = (new GuzzleHttp\Client)->get('https://publicapi.avans.nl/oauth/request_token', [
+        'form_params' => [
+            'grant_type' => 'authorization_code',
+            'client_id' => 393, // Replace with Client ID
+            'secret' => '', // Replace with client secret
+            'callback_uri' => 'http://localhost/callback',
+            'code' => $request->code,
+        ]
+    ]);
+
+    session()->put('token', json_decode((string) $response->getBody(), true));
+
+    return redirect('/lol');
+});
+
+Route::get('/todos', function () {
+    $response = (new GuzzleHttp\Client)->get('http://localhost/api/todos', [
+        'headers' => [
+            'Authorization' => 'Bearer '.session()->get('token.access_token')
+        ]
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
 });
