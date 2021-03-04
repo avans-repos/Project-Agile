5c17c5de814d12f52a463c9643182e71617d5378
Jaap <jaap@mrproper.dev>
2021-03-04 12:55:32 +0100

    Fix url

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-use Illuminate\Support\Facades\Route;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function ()
 {
@@ -9,21 +10,29 @@ Route::get('/test', function ()
 
 
 Route::get('/callback', function () {
-    $query = http_build_query([
-        'client_id' => $_ENV["AVANS_ID"], // Replace with Client ID
-        'redirect_uri' => 'http://localhost/callback',
-        'response_type' => 'code',
-        'scope' => ''
-    ]);
+    $oauth = new OAuth($_ENV["AVANS_KEY"], $_ENV["AVANS_SECRET"],OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_FORM);
+    $oauth->setToken($_REQUEST['oauth_token'], $_SESSION["tokenInfo"]["oauth_token_secret"]);
+    $accessTokenInfo = $oauth->getAccessToken("https://publicapi.avans.nl/oauth/access_token");
+    $_SESSION["accessToken"] = $accessTokenInfo;
+    unset($_SESSION["tokenInfo"]); //Its work is done
 
-    return redirect('https://publicapi.avans.nl/oauth/saml.php?'.$query);
+    //GET BASIC DATA FOR TESTING
+    $oauth = new OAuth($_ENV["AVANS_KEY"], $_ENV["AVANS_SECRET"],OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_FORM);
+    $oauth->setToken($_REQUEST['oauth_token'], $_SESSION["tokenInfo"]["oauth_token_secret"]);
+
+    $oauth->fetch("https://publicapi.avans.nl/oauth/api/user");
+    $data = $oauth->getLastResponse();
+    $requestInfo = $oauth->getLastResponseInfo();
+    $responseHeaders = $oauth->getLastResponseHeaders();
+    die($data);
+
 });
 
 Route::get('/', function (Request $request) {
 
     $oauth = new OAuth($_ENV["AVANS_KEY"], $_ENV["AVANS_SECRET"],OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_FORM);
     $oauth->disableSSLChecks();
-    $requestTokenInfo = $oauth->getRequestToken("https://publicapi.avans.nl/oauth/request_token", "http://localhost:63342/Project-Agile/server.php");
+    $requestTokenInfo = $oauth->getRequestToken("https://publicapi.avans.nl/oauth/request_token", "http://127.0.0.1:8000/callback");
 
     $_SESSION["tokenInfo"] = $requestTokenInfo;
     return redirect("https://publicapi.avans.nl/oauth/saml.php?oauth_token=".$requestTokenInfo["oauth_token"]);
