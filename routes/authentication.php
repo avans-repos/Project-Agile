898d04b26934a55ae23107c598f04940abb5ba99
Job <LordChunk@users.noreply.github.com>
2021-03-05 13:15:26 +0100

    Moved authentication onto it's own route

---
+++ routes/authentication.php
@@ -0,0 +1,34 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/authentication/callback', function () {
+    $oauth = new OAuth($_ENV["AVANS_KEY"], $_ENV["AVANS_SECRET"],OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_FORM);
+    $oauth->disableSSLChecks();
+    $oauth->setToken($_REQUEST['oauth_token'], $_SESSION["tokenInfo"]["oauth_token_secret"]);
+    $accessTokenInfo = $oauth->getAccessToken($_ENV['AVANS_ENDPOINT']."/authentication/access_token");
+    $_SESSION["accessToken"] = $accessTokenInfo;
+    unset($_SESSION["tokenInfo"]); //Its work is done
+
+    return redirect('/authentication/datafetch');
+});
+
+Route::get('/authentication', function (Request $request) {
+    $oauth = new OAuth($_ENV["AVANS_KEY"], $_ENV["AVANS_SECRET"],OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_FORM);
+    $oauth->disableSSLChecks();
+    $requestTokenInfo = $oauth->getRequestToken($_ENV['AVANS_ENDPOINT']."/authentication/request_token", $_ENV['AVANS_REDIRECT_URI']."/authentication/callback");
+
+    $_SESSION["tokenInfo"] = $requestTokenInfo;
+    return redirect($_ENV['AVANS_ENDPOINT']."/saml.php?oauth_token=".$requestTokenInfo["oauth_token"]);
+});
+
+Route::get('/authentication/datafetch', function () {
+    if(!isset($_SESSION["accessToken"])) return redirect("/authentication");
+    $oauth = new OAuth($_ENV["AVANS_KEY"], $_ENV["AVANS_SECRET"],OAUTH_SIG_METHOD_HMACSHA1);
+    $oauth->setToken($_SESSION["accessToken"]["oauth_token"],$_SESSION["accessToken"]["oauth_token_secret"]);
+    $oauth->disableSSLChecks();
+
+    $oauth->fetch($_ENV['AVANS_ENDPOINT']."/people/@me?format=json");
+    $data = $oauth->getLastResponse();
+    die($data);
+});
--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,22 @@
 <?php
-session_start();
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function ()
-{
-    return "hello world";
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
 });
 
 
-Route::get('/callback', function () {
-    $oauth = new OAuth($_ENV["AVANS_KEY"], $_ENV["AVANS_SECRET"],OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_FORM);
-    $oauth->disableSSLChecks();
-    $oauth->setToken($_REQUEST['oauth_token'], $_SESSION["tokenInfo"]["oauth_token_secret"]);
-    $accessTokenInfo = $oauth->getAccessToken($_ENV['AVANS_ENDPOINT']."/access_token");
-    $_SESSION["accessToken"] = $accessTokenInfo;
-    unset($_SESSION["tokenInfo"]); //Its work is done
-
-    return redirect('/datafetch');
-});
-
-Route::get('/', function (Request $request) {
-    $oauth = new OAuth($_ENV["AVANS_KEY"], $_ENV["AVANS_SECRET"],OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_FORM);
-    $oauth->disableSSLChecks();
-    $requestTokenInfo = $oauth->getRequestToken($_ENV['AVANS_ENDPOINT']."/request_token", $_ENV['AVANS_REDIRECT_URI']."/callback");
-
-    $_SESSION["tokenInfo"] = $requestTokenInfo;
-    return redirect($_ENV['AVANS_ENDPOINT']."/saml.php?oauth_token=".$requestTokenInfo["oauth_token"]);
-});
-
-Route::get('/datafetch', function () {
-    if(!isset($_SESSION["accessToken"])) return redirect("/");
-    $oauth = new OAuth($_ENV["AVANS_KEY"], $_ENV["AVANS_SECRET"],OAUTH_SIG_METHOD_HMACSHA1);
-    $oauth->setToken($_SESSION["accessToken"]["oauth_token"],$_SESSION["accessToken"]["oauth_token_secret"]);
-    $oauth->disableSSLChecks();
-
-    $oauth->fetch($_ENV['AVANS_ENDPOINT']."/people/@me?format=json");
-    $data = $oauth->getLastResponse();
-    die($data);
-});
+// Import authentication handler
+require __DIR__.'/authentication.php';
