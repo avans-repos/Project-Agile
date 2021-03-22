af0eb12938a812844eb03c48f1ad593109aa2360
MartijnAmbagtsheer <63644928+MartijnAmbagtsheer@users.noreply.github.com>
2021-03-22 21:18:45 +0100

    wrote tests for validation of projectgroup create/edit form

---
+++ tests/Feature/ProjectgroupCreateTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Requests\ProjectgroupRequest;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class ProjectgroupCreateTest extends TestCase
+{
+
+
+
+
+  use RefreshDatabase;
+
+
+  public function setUp() : void
+  {
+    parent::setUp();
+    $this->rules     = (new ProjectgroupRequest())->rules();
+    $this->validator = $this->app['validator'];
+    $user = new User([
+      'id' => 1,
+      'name' => 'test'
+    ]);
+    $this->be($user);
+  }
+
+  /**
+   * @return void
+   */
+  public function test_projectgroup_create_screen_can_be_rendered()
+  {
+    $this->assertAuthenticated();
+    $response = $this->get('/projectgroup/create');
+    $response->assertStatus(200);
+  }
+
+  /**
+   * @return void
+   */
+  public function test_projectgroup_create_fails_no_name()
+  {
+    $response = $this
+      ->post(route('projectgroup.store'), [
+        'name' => ''
+      ]);
+    $response->assertSessionHasErrors([
+      'name'
+    ]);
+  }
+
+
+  /**
+   * @return void
+   */
+  public function test_projectgroup_create_fails_name_too_long()
+  {
+    $response = $this
+      ->post(route('projectgroup.store'), [
+        'name' => Str::random(101)
+      ]);
+    $response->assertSessionHasErrors([
+      'name'
+    ]);
+  }
+
+
+  /**
+   * @return void
+   */
+  public function test_projectgroup_create_success()
+  {
+
+    $name = Str::random(100);
+
+    $response = $this
+      ->post(route('projectgroup.store'), [
+        'name' => $name
+      ]);
+    $response->assertSessionHasNoErrors();
+
+    $this->assertDatabaseHas('projectgroups', [
+      'name' => $name
+    ]);
+  }
+}
