a70417422e3f578f7d646878e135862ccb9ee724
Jaap <jaap@mrproper.dev>
2021-03-23 15:07:17 +0100

    Random datetime for seeds

--- database/seeders/NotesSeeder.php
+++ database/seeders/NotesSeeder.php
@@ -17,26 +17,31 @@ class NotesSeeder extends Seeder
       'description' => $faker->text(300),
       'creator' => 1,
       'contact' => 1,
+      'creation' => $faker->dateTimeBetween('-1 day', 'now')
     ]);
     DB::table('notes')->insert([
       'description' => $faker->text(200),
       'creator' => 2,
       'contact' => 1,
+      'creation' => $faker->dateTimeBetween('-1 day', 'now')
     ]);
     DB::table('notes')->insert([
       'description' => $faker->text(250),
       'creator' => 3,
       'contact' => 1,
+      'creation' => $faker->dateTimeBetween('-1 day', 'now')
     ]);
     DB::table('notes')->insert([
       'description' => $faker->text(30),
       'creator' => 4,
       'contact' => 1,
+      'creation' => $faker->dateTimeBetween('-1 day', 'now')
     ]);
     DB::table('notes')->insert([
       'description' => $faker->text(1000),
       'creator' => 5,
       'contact' => 1,
+      'creation' => $faker->dateTimeBetween('-1 day', 'now')
     ]);
   }
 }
