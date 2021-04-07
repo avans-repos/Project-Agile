4fa5223af9d2cdab33597da46bc2654d2be8177c
marijn kieboom <63717280+marijnkieboom@users.noreply.github.com>
2021-04-07 17:30:04 +0200

    repaired seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,10 @@ class DatabaseSeeder extends Seeder
     $this->call(RoleSeeder::class);
     $this->call(UserSeeder::class);
     $this->call(GenderSeeder::class);
+    $this->call(AddressSeeder::class);
+    $this->call(CompanySeeder::class);
     $this->call(ContactTypeSeeder::class);
     $this->call(ContactSeeder::class);
-    $this->call(CompanySeeder::class);
     $this->call(NotesSeeder::class);
     $this->call(ProjectgroupSeeder::class);
   }
