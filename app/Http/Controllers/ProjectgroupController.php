fbcae65b3d9e9a75dce17b77e083c884d63fa957
marijn kieboom <63717280+marijnkieboom@users.noreply.github.com>
2021-04-23 08:44:35 +0200

    contacts can be added to a project group

--- app/Http/Controllers/ProjectgroupController.php
+++ app/Http/Controllers/ProjectgroupController.php
@@ -61,14 +61,19 @@ class ProjectgroupController extends Controller
 
     $students = User::role('Student')->get();
     $teachers = User::role('Teacher')->get();
+    $contacts = Contact::all();
     $projects = Project::all();
 
-    $assigned = null;
+    $assignedUsers = null;
+    $assignedContacts = null;
+
     return view('projectgroup.manage')
       ->with('projectgroup', $projectgroup)
       ->with('teachers', $teachers)
       ->with('students', $students)
-      ->with('assigned', $assigned)
+      ->with('contacts', $contacts)
+      ->with('assignedUsers', $assignedUsers)
+      ->with('assignedContacts', $assignedContacts)
       ->with('projects', $projects)
       ->with('action', 'store');
   }
@@ -84,9 +89,21 @@ class ProjectgroupController extends Controller
     $request->validated();
     $id = Projectgroup::create($request->all())->id;
 
-    if (isset($request->assigned)) {
-      foreach ($request->assigned as $assigned) {
-        DB::insert('INSERT INTO projectgroup_has_users (userid,projectgroupid) VALUES (?,?)', [$assigned, $id]);
+    // fill in all the users (students and teachers)
+    if (isset($request->assignedUsers))
+    {
+      foreach ($request->assignedUsers as $assignedUser)
+      {
+        DB::insert('INSERT INTO projectgroup_has_users (userid,projectgroupid) VALUES (?,?)', [$assignedUser, $id]);
+      }
+    }
+
+    // fill in all the contactpersons
+    if (isset($request->assignedContacts))
+    {
+      foreach ($request->assignedContacts as $assignedContact)
+      {
+        DB::insert('INSERT INTO projectgroup_has_contacts (contactid,projectgroupid) VALUES (?,?)', [$assignedContact, $id]);
       }
     }
 
@@ -97,30 +114,43 @@ class ProjectgroupController extends Controller
    * Show the form for editing the specified resource.
    *
    * @param \App\Models\Projectgroup $projectgroup
-   * @return \Illuminate\Http\Response
+   * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
    */
   public function edit(Projectgroup $projectgroup)
   {
 
     $students = User::role('student')->get();
     $teachers = User::role('teacher')->get();
+    $contacts = Contact::all();
     $projects = Project::all();
 
-    $assigned =
+    $assignedUsers =
       DB::table('projectgroup_has_users')
         ->where('projectgroupid', '=', $projectgroup->id)
         ->join('users', 'projectgroup_has_users.userid', '=', 'users.id')
         ->get('users.id') ?? [];
 
-    $assigned = array_map(function ($teacher) {
+    $assignedUsers = array_map(function ($teacher) {
       return $teacher->id;
-    }, json_decode($assigned));
+    }, json_decode($assignedUsers));
+
+    $assignedContacts =
+      DB::table('projectgroup_has_contacts')
+        ->where('projectgroupid', '=', $projectgroup->id)
+        ->join('contacts', 'projectgroup_has_contacts.contactid', '=', 'contacts.id')
+        ->get('contacts.id') ?? [];
+
+    $assignedContacts = array_map(function ($contact) {
+      return $contact->id;
+    }, json_decode($assignedContacts));
 
     return view('projectgroup.manage')
       ->with('projectgroup', $projectgroup)
       ->with('teachers', $teachers)
       ->with('students', $students)
-      ->with('assigned', $assigned)
+      ->with('contacts', $contacts)
+      ->with('assignedUsers', $assignedUsers)
+      ->with('assignedContacts', $assignedContacts)
       ->with('projects', $projects)
       ->with('action', 'update');
   }
@@ -136,31 +166,60 @@ class ProjectgroupController extends Controller
   {
     $request->validated();
 
-    if (isset($request->assigned)) {
-
-    foreach ($request->assigned as $assigned) {
-      if (
-      !DB::table('projectgroup_has_users')
-        ->where('userid', $assigned)
-        ->where('projectgroupid', $projectgroup->id)
-        ->exists()
-      ) {
-        DB::insert('INSERT INTO projectgroup_has_users (userid,projectgroupid) VALUES (?,?)', [$assigned, $projectgroup->id]);
+    // insert the connections with users (students and teachers)
+    if (isset($request->assignedUsers)) {
+      foreach ($request->assignedUsers as $assignedUser) {
+        if (
+        !DB::table('projectgroup_has_users')
+          ->where('userid', $assignedUser)
+          ->where('projectgroupid', $projectgroup->id)
+          ->exists()
+        ) {
+          DB::insert('INSERT INTO projectgroup_has_users (userid,projectgroupid) VALUES (?,?)', [$assignedUser, $projectgroup->id]);
+        }
       }
     }
-  }
+
+    // insert the connections with contactpersons
+    if (isset($request->assignedContacts)) {
+      foreach ($request->assignedContacts as $assignedContact) {
+        if (
+        !DB::table('projectgroup_has_contacts')
+          ->where('contactid', $assignedContact)
+          ->where('projectgroupid', $projectgroup->id)
+          ->exists()
+        ) {
+          DB::insert('INSERT INTO projectgroup_has_contacts (contactid,projectgroupid) VALUES (?,?)', [$assignedContact, $projectgroup->id]);
+        }
+      }
+    }
+
+    // removing unnecessary connections with users (students and teachers
     foreach (
       DB::table('projectgroup_has_users')
         ->where('projectgroupid', $projectgroup->id)
         ->get()
       as $dbvalue
     ) {
-      if (!isset($request->assigned) || !in_array($dbvalue->userid, $request->assigned)) {
+      if (!isset($request->assignedUsers) || !in_array($dbvalue->userid, $request->assignedUsers)) {
         DB::delete('DELETE FROM projectgroup_has_users WHERE userid = ? AND projectgroupid = ?', [$dbvalue->userid, $dbvalue->projectgroupid]);
       }
     }
 
+    // removing unnecessary connections with contactpersons
+    foreach (
+      DB::table('projectgroup_has_contacts')
+        ->where('projectgroupid', $projectgroup->id)
+        ->get()
+      as $dbvalue
+    ) {
+      if (!isset($request->assignedContacts) || !in_array($dbvalue->contactid, $request->assignedContacts)) {
+        DB::delete('DELETE FROM projectgroup_has_contacts WHERE contactid = ? AND projectgroupid = ?', [$dbvalue->contactid, $dbvalue->projectgroupid]);
+      }
+    }
+
     $projectgroup->update($request->all());
+
     return redirect()->route('projectgroup.index');
   }
 
