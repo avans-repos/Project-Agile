e132ebba479d9ac0cbcea1338693fe3dbacde260
MartijnAmbagtsheer <63644928+MartijnAmbagtsheer@users.noreply.github.com>
2021-04-29 11:57:28 +0200

    Edited wrong refector

--- routes/auth.php
+++ routes/auth.php
@@ -46,7 +46,7 @@ Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke
   ->middleware(['auth', 'signed', 'throttle:6,1'])
   ->name('verification.verify');
 
-Route::post('/email/verification-notificationController', [EmailVerificationNotificationController::class, 'store'])
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
   ->middleware(['auth', 'throttle:6,1'])
   ->name('verification.send');
 
