e563cb04e9493e0d231da2589e37e179bb27d4b1
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2021-05-04 16:39:42 +0200

    Update app/Models/student_has_class_room.php
    Co-authored-by: Job <LordChunk@users.noreply.github.com>

--- app/Models/student_has_class_room.php
+++ app/Models/student_has_class_room.php
@@ -14,7 +14,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class student_has_class_room extends Model
 {
-    use HasFactory;
+  use HasFactory;
   protected $table = 'student_has_class_rooms';
   protected $fillable = ['student','class_room'];
   public $timestamps = false;
