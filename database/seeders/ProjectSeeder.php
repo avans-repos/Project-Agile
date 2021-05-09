687e1c1f1876e102f9dcb570d74d1b1f8ee70f09
MartijnAmbagtsheer <63644928+MartijnAmbagtsheer@users.noreply.github.com>
2021-05-09 20:49:38 +0200

    insert to create where needed

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,14 +15,14 @@ class ProjectSeeder extends Seeder
      */
   public function run()
   {
-    DB::table('projects')->insert([
+    project::create([
       'name' => 'Interview met Vizova',
       'description' => 'Leg een interview af met Vizova en verwerk deze in een verslag.',
       'deadline' => new \DateTime('10/10/2021'),
       'notes' => 'De rubric staat op BlackBoard.',
     ]);
 
-    DB::table('projects')->insert([
+    project::create([
       'name' => 'Maak een plan van aanpak voor een bedrijf.',
       'description' => 'Plan van aanpak maken voor bedrijf X. Dit moet in tweetallen.',
       'deadline' => new \DateTime('10/10/2021'),
--- database/seeders/ProjectgroupSeeder.php
+++ database/seeders/ProjectgroupSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Projectgroup;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -15,12 +16,12 @@ class ProjectgroupSeeder extends Seeder
      */
     public function run()
     {
-      DB::table('projectgroups')->insert([
+      projectgroup::create([
         'name' => 'IN01 - Groep A1',
         'project' => 1
       ]);
 
-      DB::table('projectgroups')->insert([
+      projectgroup::create([
         'name' => 'IN01 - Groep B2',
         'project' => 2
       ]);
