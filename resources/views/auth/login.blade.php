795252ea45563da5b6e10e4ddb1343824001a496
jeroenvermaat2000 <jeroenvermaat2000@gmail.com>
2021-05-16 14:07:04 +0200

    engels naar nederlands

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
 
       <!-- Password -->
       <div class="mt-4">
-        <x-label for="password" :value="__('Password')" />
+        <x-label for="password" :value="__('Wachtwoord')" />
 
         <x-input id="password" class="block mt-1 w-full"
                  type="password"
