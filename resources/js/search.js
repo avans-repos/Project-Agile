ebd5b42e4b79c15b3f6eae90b2c0804533dea1cc
Tom Cornelissen <tomcornelissennl@gmail.com>
2021-05-25 10:42:07 +0200

    Prettier

--- resources/js/search.js
+++ resources/js/search.js
@@ -1,36 +1,30 @@
-document.getElementById("searchInput").addEventListener("keyup", singleSearch);
+document.getElementById('searchInput').addEventListener('keyup', singleSearch);
 
 function singleSearch() {
-
-  // Declare variables 
-  var input = document.getElementById("searchInput");
+  // Declare variables
+  var input = document.getElementById('searchInput');
   var filter = input.value.toUpperCase();
-  var table = document.getElementById("searchTable");
-  var trs = table.tBodies[0].getElementsByTagName("tr");
+  var table = document.getElementById('searchTable');
+  var trs = table.tBodies[0].getElementsByTagName('tr');
 
   // Loop through first tbody's rows
   for (var i = 0; i < trs.length; i++) {
-
     // define the row's cells
-    var tds = trs[i].getElementsByTagName("td");
+    var tds = trs[i].getElementsByTagName('td');
 
     // hide the row
-    trs[i].style.display = "none";
+    trs[i].style.display = 'none';
 
     // loop through row cells
     for (var i2 = 0; i2 < tds.length; i2++) {
-
       // if there's a match
       if (tds[i2].innerHTML.toUpperCase().indexOf(filter) > -1) {
-
         // show the row
-        trs[i].style.display = "";
+        trs[i].style.display = '';
 
         // skip to the next row
         continue;
-
       }
     }
   }
-
 }
