80f941680b65ca71b75f98e9074f6c7e7c6d546d
Jaap <jaap@mrproper.dev>
2021-05-26 13:57:46 +0200

    Max height

--- resources/views/mailformat/send.blade.php
+++ resources/views/mailformat/send.blade.php
@@ -114,7 +114,7 @@
   <fieldset class="mt-5">
     <legend>Contacten toevoegen</legend>
     <input type="text" id="filterContactInput" onkeyup="filterContacts()" placeholder="Zoek naar contacten" title="Typ een naam">
-    <ul class="list-group mt-2 mb-2 scroll max-h-screen" id="contactList">
+    <ul class="list-group mt-2 mb-2 scroll max-h-96" id="contactList">
       @foreach($contacts as $contact)
         <li class="list-group-item list-group-item-action" id="{{$contact->id}}">
           <div class="container">
