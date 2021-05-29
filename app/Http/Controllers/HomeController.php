96a5e42fd9d153572af920dff7acb5914a840f81
Jaap <jaap@mrproper.dev>
2021-05-29 15:59:35 +0200

    Remove unused import

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-  public function index(Request $request)
+  public function index()
   {
     $actionPoints = Auth::user()
       ->actionpoints()
