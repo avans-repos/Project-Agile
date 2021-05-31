81dff32b31b78e473e18815d7d3af7c85ea30d2f
Job <LordChunk@users.noreply.github.com>
2021-05-31 19:43:38 +0200

    Ran prettier

--- database/seeders/ProjectgroupSeeder.php
+++ database/seeders/ProjectgroupSeeder.php
@@ -17,31 +17,41 @@ class ProjectgroupSeeder extends Seeder
    */
   public function run()
   {
-    for($i = 0; $i < 50; $i++) {
-      $projectId = Project::all()->random(1)->first()->id;
+    for ($i = 0; $i < 50; $i++) {
+      $projectId = Project::all()
+        ->random(1)
+        ->first()->id;
       $group = ProjectGroup::create([
         'name' => 'IN01 - Groep A' . $i,
         'project' => $projectId,
       ]);
 
       for ($i2 = 0; $i2 < random_int(0, 3); $i2++) {
-        $userId = User::role('Student')->get()->random(1)->first()->id;
-        if(!$group
-          ->users()
-          ->where('user_id', $userId)
-          ->exists()){
-          $group
-            ->users()->attach($userId);
+        $userId = User::role('Student')
+          ->get()
+          ->random(1)
+          ->first()->id;
+        if (
+          !$group
+            ->users()
+            ->where('user_id', $userId)
+            ->exists()
+        ) {
+          $group->users()->attach($userId);
         }
       }
-      $group
-        ->users()
-        ->attach(User::role('Teacher')->get()->random(1)->first()->id);
+      $group->users()->attach(
+        User::role('Teacher')
+          ->get()
+          ->random(1)
+          ->first()->id
+      );
 
-      $group
-        ->contacts()
-        ->attach(Contact::all()->random(1)->first()->id);
-
+      $group->contacts()->attach(
+        Contact::all()
+          ->random(1)
+          ->first()->id
+      );
     }
   }
 }
