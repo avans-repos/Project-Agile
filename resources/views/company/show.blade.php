d15bdaba064aa917ca5b24f8374da84e9bc50201
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2021-06-01 17:32:56 +0200

    Update resources/views/company/show.blade.php

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -199,7 +199,7 @@
               @if($contact->notes()->get()->count() > 1)
                 <button class="read-more-button" type="button" data-bs-toggle="collapse"
                         data-bs-target=".extra-notes-{{$contact->id}}">Meer lezen...</button>
-                <div class="collapse extra-notes" id="notes-{{$contact->id}}">
+                <div class="collapse extra-notes-{{$contact->id}}" id="notes-{{$contact->id}}">
                   <div class="w-100 h-50 notes-height">
                     @foreach($contact->notes()->get() as $note)
                       @continue($loop->index == 0)
