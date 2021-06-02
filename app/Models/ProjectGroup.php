2dc23998c2d37d6c31f846154e11a06ccaf00cb2
jeroenvermaat2000 <jeroenvermaat2000@gmail.com>
2021-06-02 17:41:57 +0200

    melding van gekoppeld project bij verwijderen projectgroep

--- app/Models/ProjectGroup.php
+++ app/Models/ProjectGroup.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\contact\Contact;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -10,6 +11,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @method static where(string $string, mixed $id)
+ * @property mixed project
+ * * @method static Builder|ProjectGroup whereProject($value)
  */
 class ProjectGroup extends Model
 {
@@ -18,6 +21,17 @@ class ProjectGroup extends Model
   protected $primaryKey = 'id';
   protected $fillable = ['name', 'project'];
   protected $dates = ['deleted_at'];
+
+  public function getDeleteText() : string
+  {
+    $project = Project::where('id',$this->project)->pluck('name')->first();
+    $text = "";
+    if ($project !== null){
+      $text = "Er is een project aan deze projectgroep gekoppeld: ";
+      $text .= ' ' . $project;
+    }
+    return $text;
+  }
 
   public function users(): BelongsToMany
   {
