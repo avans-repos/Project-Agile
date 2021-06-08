019d00653bfcc380f6b39a348a8733a8010c6a47
Job <LordChunk@users.noreply.github.com>
2021-06-08 17:55:59 +0200

    Made dist files s u p e r  pretty

--- public/js/search.js
+++ public/js/search.js
@@ -1,1 +1,13 @@
-document.getElementById("searchInput").addEventListener("keyup",(function(){for(var e=document.getElementById("searchInput").value.toUpperCase(),t=document.getElementById("searchTable").tBodies[0].getElementsByTagName("tr"),n=0;n<t.length;n++){var a=t[n].getElementsByTagName("td");t[n].style.display="none";for(var d=0;d<a.length;d++)a[d].innerHTML.toUpperCase().indexOf(e)>-1&&(t[n].style.display="")}}));
+document.getElementById('searchInput').addEventListener('keyup', function () {
+  for (
+    var e = document.getElementById('searchInput').value.toUpperCase(),
+      t = document.getElementById('searchTable').tBodies[0].getElementsByTagName('tr'),
+      n = 0;
+    n < t.length;
+    n++
+  ) {
+    var a = t[n].getElementsByTagName('td');
+    t[n].style.display = 'none';
+    for (var d = 0; d < a.length; d++) a[d].innerHTML.toUpperCase().indexOf(e) > -1 && (t[n].style.display = '');
+  }
+});
