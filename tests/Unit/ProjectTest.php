2475fe2a434f3ebaa1e943337bec91a1a183e8fa
jeroenvermaat2000 <jeroenvermaat2000@gmail.com>
2021-06-09 16:22:05 +0200

    seeder toegevoegd

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -2,22 +2,37 @@
 
 namespace Tests\Unit;
 
+use App\Http\Requests\ProjectRequest;
 use App\Models\Project;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\CreatesApplication;
 use Tests\TestCase;
 
 class ProjectTest extends TestCase
 {
+  use CreatesApplication;
   use RefreshDatabase;
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_getDeleteText_No_Relations()
-    {
-      $newProject = new Project();
-      $newProject->name = 'test1';
-      $this->assertEquals('Weet u zeker dat u "test1" wilt verwijderen<br>', $newProject->getDeleteText());
-    }
+
+  protected $seed = true;
+
+  public function setUp(): void
+  {
+    parent::setUp();
+    $this->rules = (new ProjectRequest())->rules();
+    $this->validator = $this->app['validator'];
+    $user = new User([
+      'id' => 1,
+      'name' => 'test',
+    ]);
+
+    $this->be($user);
+  }
+  public function test_getDeleteText_No_Relations()
+  {
+    $newProject = new Project();
+    $newProject->name = 'test1';
+    $this->assertEquals('Weet u zeker dat u "test1" wilt verwijderen<br>', $newProject->getDeleteText());
+  }
+
 }
