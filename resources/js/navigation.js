790e2e8f7a8978322f05d5ff19ef82450774e3b8
Tom Cornelissen <tomcornelissennl@gmail.com>
2021-06-10 10:02:11 +0200

    Run prettier

--- resources/js/navigation.js
+++ resources/js/navigation.js
@@ -1,59 +1,58 @@
 setupDropdown(
-    document.getElementById('avans-menu'),
-    document.getElementById('avans-mail-dropdown'),
-    document.getElementById('avans-mail-dropdown-button')
+  document.getElementById('avans-menu'),
+  document.getElementById('avans-mail-dropdown'),
+  document.getElementById('avans-mail-dropdown-button')
 );
 
 setupDropdown(
-    document.getElementById('avans-menu'),
-    document.getElementById('avans-user-dropdown'),
-    document.getElementById('avans-user-dropdown-button')
+  document.getElementById('avans-menu'),
+  document.getElementById('avans-user-dropdown'),
+  document.getElementById('avans-user-dropdown-button')
 );
 
 let timeout;
 
-function setupDropdown(menu, list, anchor)
-{
-    list.style.height = '0px';
+function setupDropdown(menu, list, anchor) {
+  list.style.height = '0px';
 
-    menu.addEventListener('mouseleave', function() {collapse(list, anchor, 3)});
-    anchor.addEventListener('mouseenter', function() {expand(list, anchor)});
-    anchor.addEventListener('mouseleave', function(event) {
-        if (document.elementFromPoint(event.clientX, event.clientY) == list.childNodes[1]) return;
-        
-        collapse(list, anchor, 0);
-    });
+  menu.addEventListener('mouseleave', function () {
+    collapse(list, anchor, 3);
+  });
+  anchor.addEventListener('mouseenter', function () {
+    expand(list, anchor);
+  });
+  anchor.addEventListener('mouseleave', function (event) {
+    if (document.elementFromPoint(event.clientX, event.clientY) == list.childNodes[1]) return;
+
+    collapse(list, anchor, 0);
+  });
 }
 
-function expand(list, anchor)
-{
-    clearInterval(timeout);
-    timeout = setInterval(function() {
-        console.log("expanding");
-        let position = anchor.getBoundingClientRect();
-        list.style.left = Math.round(position.left) + "px";
-        list.style.top = Math.round(position.bottom) + "px";
-        list.style.display = 'flex';
-        if (parseInt(list.style.height) < 75*2)
-            list.style.height = parseInt(list.style.height) + 20 + "px";
-        else
-            list.style.height = 75*2;
-    }, 16);
+function expand(list, anchor) {
+  clearInterval(timeout);
+  timeout = setInterval(function () {
+    console.log('expanding');
+    let position = anchor.getBoundingClientRect();
+    list.style.left = Math.round(position.left) + 'px';
+    list.style.top = Math.round(position.bottom) + 'px';
+    list.style.display = 'flex';
+    if (parseInt(list.style.height) < 75 * 2) list.style.height = parseInt(list.style.height) + 20 + 'px';
+    else list.style.height = 75 * 2;
+  }, 16);
 }
 
-function collapse(list, anchor, compensation)
-{
-    clearInterval(timeout);
-    let collapseTimeout = setInterval(function() {
-        console.log("collapsing");
-        if (parseInt(list.style.height) >= 20) {
-            let position = anchor.getBoundingClientRect();
-            list.style.left = Math.round(position.left) + "px";
-            list.style.top = Math.round(position.bottom) - compensation + "px";
-            list.style.height = parseInt(list.style.height) - 20 + "px";
-        } else {
-            list.style.display = 'none';
-            clearInterval(collapseTimeout);
-        }
-    }, 16);
+function collapse(list, anchor, compensation) {
+  clearInterval(timeout);
+  let collapseTimeout = setInterval(function () {
+    console.log('collapsing');
+    if (parseInt(list.style.height) >= 20) {
+      let position = anchor.getBoundingClientRect();
+      list.style.left = Math.round(position.left) + 'px';
+      list.style.top = Math.round(position.bottom) - compensation + 'px';
+      list.style.height = parseInt(list.style.height) - 20 + 'px';
+    } else {
+      list.style.display = 'none';
+      clearInterval(collapseTimeout);
+    }
+  }, 16);
 }
