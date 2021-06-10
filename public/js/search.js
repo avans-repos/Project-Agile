02dde417a7935f61261612d1eced44dd94c21ee8
Tom Cornelissen <tomcornelissennl@gmail.com>
2021-06-10 09:33:33 +0200

    Merge

--- public/js/search.js
+++ public/js/search.js
@@ -1,11 +1,33 @@
-document.querySelectorAll('[id=searchInput]').forEach(function (e) {
-  e.addEventListener('keyup', function () {
-    !(function (e) {
-      for (var t = e.value.toUpperCase(), n = e.nextElementSibling.tBodies[0].getElementsByTagName('tr'), a = 0; a < n.length; a++) {
-        var l = n[a].getElementsByTagName('td');
-        n[a].style.display = 'none';
-        for (var r = 0; r < l.length; r++) l[r].innerHTML.toUpperCase().indexOf(t) > -1 && (n[a].style.display = '');
-      }
-    })(e);
+/******/ (() => { // webpackBootstrap
+/*!********************************!*\
+  !*** ./resources/js/search.js ***!
+  \********************************/
+document.querySelectorAll('[id=searchInput]').forEach(function (input) {
+  input.addEventListener('keyup', function () {
+    singleSearch(input);
   });
 });
+
+function singleSearch(input) {
+  // Declare variables
+  var filter = input.value.toUpperCase();
+  var table = input.nextElementSibling;
+  var trs = table.tBodies[0].getElementsByTagName('tr'); // Loop through first tbody's rows
+
+  for (var i = 0; i < trs.length; i++) {
+    // define the row's cells
+    var tds = trs[i].getElementsByTagName('td'); // hide the row
+
+    trs[i].style.display = 'none'; // loop through row cells
+
+    for (var i2 = 0; i2 < tds.length; i2++) {
+      // if there's a match
+      if (tds[i2].innerHTML.toUpperCase().indexOf(filter) > -1) {
+        // show the row
+        trs[i].style.display = '';
+      }
+    }
+  }
+}
+/******/ })()
+;
