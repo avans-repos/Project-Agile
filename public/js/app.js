590b803de69ec2c53c83d28c0d5f90b5ab1c596d
Tom Cornelissen <tomcornelissennl@gmail.com>
2021-06-10 12:31:51 +0200

    Improve menu responsiveness

--- public/js/app.js
+++ public/js/app.js
@@ -3984,25 +3984,25 @@ function setupDropdown(menu, list, anchor) {
 function expand(list, anchor) {
   clearInterval(timeout);
   timeout = setInterval(function () {
-    console.log("expanding");
+    console.log('expanding');
     var position = anchor.getBoundingClientRect();
-    list.style.left = Math.round(position.left) + "px";
-    list.style.top = Math.round(position.bottom) + "px";
+    list.style.left = Math.round(position.left) + 'px';
+    list.style.top = Math.round(position.bottom) + 'px';
     list.style.display = 'flex';
-    if (parseInt(list.style.height) < 75 * 2) list.style.height = parseInt(list.style.height) + 20 + "px";else list.style.height = 75 * 2;
+    if (parseInt(list.style.height) < 75 * 2) list.style.height = parseInt(list.style.height) + 20 + 'px';else list.style.height = 75 * 2;
   }, 16);
 }
 
 function collapse(list, anchor, compensation) {
   clearInterval(timeout);
   var collapseTimeout = setInterval(function () {
-    console.log("collapsing");
+    console.log('collapsing');
 
     if (parseInt(list.style.height) >= 20) {
       var position = anchor.getBoundingClientRect();
-      list.style.left = Math.round(position.left) + "px";
-      list.style.top = Math.round(position.bottom) - compensation + "px";
-      list.style.height = parseInt(list.style.height) - 20 + "px";
+      list.style.left = Math.round(position.left) + 'px';
+      list.style.top = Math.round(position.bottom) - compensation + 'px';
+      list.style.height = parseInt(list.style.height) - 20 + 'px';
     } else {
       list.style.display = 'none';
       clearInterval(collapseTimeout);
--- public/js/search.js
+++ public/js/search.js
@@ -1,34 +1,33 @@
-/******/ (() => {
-  // webpackBootstrap
-  /*!********************************!*\
+/******/ (() => { // webpackBootstrap
+/*!********************************!*\
   !*** ./resources/js/search.js ***!
   \********************************/
-  document.querySelectorAll('[id=searchInput]').forEach(function (input) {
-    input.addEventListener('keyup', function () {
-      singleSearch(input);
-    });
+document.querySelectorAll('[id=searchInput]').forEach(function (input) {
+  input.addEventListener('keyup', function () {
+    singleSearch(input);
   });
+});
 
-  function singleSearch(input) {
-    // Declare variables
-    var filter = input.value.toUpperCase();
-    var table = input.nextElementSibling;
-    var trs = table.tBodies[0].getElementsByTagName('tr'); // Loop through first tbody's rows
+function singleSearch(input) {
+  // Declare variables
+  var filter = input.value.toUpperCase();
+  var table = input.nextElementSibling;
+  var trs = table.tBodies[0].getElementsByTagName('tr'); // Loop through first tbody's rows
 
-    for (var i = 0; i < trs.length; i++) {
-      // define the row's cells
-      var tds = trs[i].getElementsByTagName('td'); // hide the row
+  for (var i = 0; i < trs.length; i++) {
+    // define the row's cells
+    var tds = trs[i].getElementsByTagName('td'); // hide the row
 
-      trs[i].style.display = 'none'; // loop through row cells
+    trs[i].style.display = 'none'; // loop through row cells
 
-      for (var i2 = 0; i2 < tds.length; i2++) {
-        // if there's a match
-        if (tds[i2].innerHTML.toUpperCase().indexOf(filter) > -1) {
-          // show the row
-          trs[i].style.display = '';
-        }
+    for (var i2 = 0; i2 < tds.length; i2++) {
+      // if there's a match
+      if (tds[i2].innerHTML.toUpperCase().indexOf(filter) > -1) {
+        // show the row
+        trs[i].style.display = '';
       }
     }
   }
-  /******/
-})();
+}
+/******/ })()
+;
