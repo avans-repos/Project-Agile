0c464da5ca24a740feac56a819a5f87988381783
Jaap <jaap@mrproper.dev>
2021-06-21 11:41:33 +0200

    Email tag

--- resources/views/mailformat/form.blade.php
+++ resources/views/mailformat/form.blade.php
@@ -31,7 +31,7 @@
             <div class="card card-body p-3 mb-1">
               <ul class="list-group list-group my-2">
                 @foreach($tags as $tag)
-                  <li class="list-group-item d-flex"><h5 class="mb-1 text-primary col-sm-4">{{'{'.$tag->tag .'}'}}</h5>
+                  <li class="list-group-item d-flex"><h5 class="mb-1 text-primary col-sm-4" onclick="insertAtCaret(`{{'{'.$tag->tag.'}'}}`)">{{'{'.$tag->tag .'}'}}</h5>
                     <small class="col-sm-8">{{$tag->description}}</small></li>
                 @endforeach
               </ul>
@@ -52,3 +52,21 @@
   </fieldset>
   <input class="btn btn-primary" type="submit" value="Standaardtekst {{$formActionViewName}}">
 </form>
+
+<script>
+  function insertAtCaret(text) {
+    let txtarea = document.getElementById('body');
+
+    let scrollPos = txtarea.scrollTop;
+    let caretPos = txtarea.selectionStart;
+
+    let front = (txtarea.value).substring(0, caretPos);
+    let back = (txtarea.value).substring(txtarea.selectionEnd, txtarea.value.length);
+    txtarea.value = front + text + back;
+    caretPos = caretPos + text.length;
+    txtarea.selectionStart = caretPos;
+    txtarea.selectionEnd = caretPos;
+    txtarea.focus();
+    txtarea.scrollTop = scrollPos;
+  }
+</script>
